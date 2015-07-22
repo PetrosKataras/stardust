8f81694aa4068c3cc6c3377ea717e358a4d89db8
Petros Kataras <petros.kataras@aec.at>
2015-07-22 14:10:09 +0200

    Update EQ build script.

--- utils/linux/build_EQ.sh
+++ utils/linux/build_EQ.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-EQ_ROOT_DIR=$PWD/../../libs/equalizer
+EQ_ROOT_DIR=$PWD/../../libs
 BUILD_TYPE=Release
 
 if [ -n "$1" ];then
@@ -12,11 +12,7 @@ if [ -n "$1" ];then
         BUILD_TYPE=Debug;
     elif [ "$1" = "clobber" ];then
         echo "Clobber - Cleaning Equalizer and dependencies......................................." 
-        cd $EQ_ROOT_DIR/Lunchbox && rm -rf Build;
-        cd $EQ_ROOT_DIR/vmmlib && rm -rf Build;
-        cd $EQ_ROOT_DIR/Pression && rm -rf Build;
-        cd $EQ_ROOT_DIR/Collage && rm -rf Build;
-        cd $EQ_ROOT_DIR/Equalizer && rm -rf Build;
+        cd $EQ_ROOT_DIR/equalizer && rm -rf Build;
         cd $EQ_ROOT_DIR && rm -rf install;
         exit 0;
     else
@@ -27,8 +23,4 @@ fi
 
 echo " Building Equalizer and its dependencies in : " $BUILD_TYPE " MODE .";
 
-cd $EQ_ROOT_DIR/Lunchbox && mkdir -p Build && cd Build && cmake .. -DCMAKE_INSTALL_PREFIX=$EQ_ROOT_DIR/install -DCMAKE_BUILD_TYPE=$BUILD_TYPE && make install;
-cd $EQ_ROOT_DIR/vmmlib && mkdir -p Build && cd Build && cmake .. -DCMAKE_INSTALL_PREFIX=$EQ_ROOT_DIR/install -DCMAKE_BUILD_TYPE=$BUILD_TYPE && make install;
-cd $EQ_ROOT_DIR/Pression && mkdir -p Build && cd Build && cmake .. -DCMAKE_INSTALL_PREFIX=$EQ_ROOT_DIR/install -DCMAKE_BUILD_TYPE=$BUILD_TYPE && make install;
-cd $EQ_ROOT_DIR/Collage && mkdir -p Build && cd Build && cmake .. -DCMAKE_INSTALL_PREFIX=$EQ_ROOT_DIR/install -DCMAKE_BUILD_TYPE=$BUILD_TYPE && make install;
-cd $EQ_ROOT_DIR/Equalizer && mkdir -p Build && cd Build && cmake .. -DCMAKE_INSTALL_PREFIX=$EQ_ROOT_DIR/install -DCMAKE_BUILD_TYPE=$BUILD_TYPE && make install;
+cd $EQ_ROOT_DIR/equalizer && mkdir -p Build && cd Build && cmake .. -DCMAKE_INSTALL_PREFIX=$EQ_ROOT_DIR/install -DCMAKE_BUILD_TYPE=$BUILD_TYPE && make install;
